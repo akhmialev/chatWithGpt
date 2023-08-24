3717f83605a6c58953de6adbb4a3db57daaa7cf3
artem <akhmialev@gmail.com>
2023-08-24 21:46:32 +0300

    added function which check count click

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import openai
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
-from tools import load_messages, save_messages, messages
+from tools import load_messages, save_messages, messages, get_user_ip, ip_message_count
 from config import API_KEY
 
 openai.api_key = API_KEY
@@ -18,9 +18,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get('/api/v1/')
-def get_request(msg: str):
+def get_request(msg: str, request: Request, message_count: int = Depends(get_user_ip)):
     load_messages()
+    client_ip = request.client.host
+
+    if message_count >= 3:
+        return {'message': 'Limit exceeded'}
+
     messages.append({'role': 'user', 'content': msg})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -29,6 +35,9 @@ def get_request(msg: str):
     answer = response.choices[0]['message']['content']
     messages.append({'role': 'assistant', 'content': answer})
     save_messages()
+
+    ip_message_count[client_ip] = message_count + 1
+    print(ip_message_count)
     return answer
 
 
--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
 import json
+from fastapi import Request
 
 messages = [
     {'role': 'system', 'content': 'Previous message'},
     {'role': 'user', 'content': ''}
 ]
+ip_message_count = {}
 
 
 def save_messages():
@@ -18,3 +20,8 @@ def load_messages():
             messages = json.load(file)
     except FileNotFoundError:
         messages = []
+
+
+def get_user_ip(request: Request):
+    client_ip = request.client.host
+    return ip_message_count.get(client_ip, 0)
