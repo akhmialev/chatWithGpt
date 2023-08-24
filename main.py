287cfc6d2425210c9152e402d87ab2349be54bd0
artem <akhmialev@gmail.com>
2023-08-24 18:21:19 +0300

    added base front page

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import openai
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from tools import load_messages, save_messages, messages
 from config import API_KEY
 
 openai.api_key = API_KEY
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/api/v1/')
 def get_request(msg: str):
     load_messages()
     messages.append({'role': 'user', 'content': msg})
-
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
     )
-
     answer = response.choices[0]['message']['content']
     messages.append({'role': 'assistant', 'content': answer})
     save_messages()
