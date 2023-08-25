e8f6869aa6245487724286600ea8862df91dee28
artem <akhmialev@gmail.com>
2023-08-25 23:16:48 +0300

    added 2 new func for save and load history of posts

--- db.py
+++ db.py
@@ -15,6 +15,7 @@ def connect_to_db():
 def check_db_max_count_ip(ip):
     """
     Функция проверяет есть ли юзер в базе и проверяет количество запросов не зарегистрированных пользователей(их 3)
+    делает проверку даты(запросы можно задать 1 раз в сутки), в новых сутках чистит базу данных.
     :param ip: user ip
     """
     db = connect_to_db()
@@ -32,13 +33,39 @@ def check_db_max_count_ip(ip):
             else:
                 return True
         else:
-            update_date = {"$set": {'count': 1, 'date': str(today)}}
+            update_date = {"$set": {'count': 1, 'date': str(today), 'messages': []}}
             collection.update_one(query, update_date)
     else:
         data = {
             'ip': ip,
             'count': 1,
-            'date': str(today)
+            'date': str(today),
+            'messages': []
         }
         collection.insert_one(data)
         return False
+
+
+def load_msg(ip):
+    """
+    Функция для загрузки истории сообщений.
+    :param ip: IP пользователя
+    """
+    db = connect_to_db()
+    collection = db.get_collection('users')
+    query = {'ip': ip}
+    user = collection.find_one(query)
+    return user['messages']
+
+
+def save_msg(ip, messages_to_add):
+    """
+    Функция для сохранения истории сообщений.
+    :param ip: IP пользователя
+    :param messages_to_add: сообщение пользователя и ответ ИИ для сохранения
+    """
+    db = connect_to_db()
+    collection = db.get_collection('users')
+    query = {'ip': ip}
+    update_query = {"$push": {"messages": {"$each": messages_to_add}}}
+    collection.update_one(query, update=update_query)
