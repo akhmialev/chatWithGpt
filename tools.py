1650b29319b961fed6cd394652df6675785b806e
artem <akhmialev@gmail.com>
2023-08-31 14:56:34 +0300

    added func hash password

--- tools.py
+++ tools.py
@@ -1,9 +1,8 @@
+from fastapi import Request
 import hashlib
 
-from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
-
-from db import check_db_max_count_ip, load_msg, save_msg, create_new_user
+from db import check_db_max_count_ip
 
 
 def add_middleware(app):
@@ -22,29 +21,6 @@ def add_middleware(app):
     return app
 
 
-def save_messages(request, msg, answer):
-    """
-        Функция для добавления в бд сообщений пользователя и ответов от ИИ.
-    :param request: для получения IP пользователя.
-    :param msg: сообщение пользователя.
-    :param answer: ответ ИИ
-    """
-    client = request.client.host
-    messages_to_add = [
-        {'role': 'user', 'content': msg},
-        {'role': 'assistant', 'content': answer}
-    ]
-    save_msg(client, messages_to_add)
-
-
-def load_messages(request):
-    """
-        Функция для загрузки истории сообщений, что бы ИИ помнил историю.
-    """
-    client = request.client.host
-    return load_msg(client)
-
-
 def get_user_ip(request: Request):
     """
         Функция берет user id из request
@@ -57,13 +33,11 @@ def get_user_ip(request: Request):
 
 
 def hash_password(password):
+    """
+        Функция для шифрования пароля.
+    :param password: пароль
+    """
     sha256_hash = hashlib.sha256()
     sha256_hash.update(password.encode('utf-8'))
     encrypted_password = sha256_hash.hexdigest()
     return encrypted_password
-
-
-def register(email, password):
-    password = hash_password(password)
-    if create_new_user(email, password):
-        return True
